d76b3fa88c4512009b5b6bb3f8570921fa343bec
NoName <contact@cedricduffournet.com>
2023-02-05 15:13:28 +0100

    add jest

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { API_URL } from '@shared/data-access/api-url.token'
+
 
 import { AppComponent } from './app.component';
 

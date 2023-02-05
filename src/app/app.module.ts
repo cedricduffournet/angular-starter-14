6b63fb3b9dd2130d86e984b87c55d5f981329463
NoName <contact@cedricduffournet.com>
2023-02-05 14:16:39 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { API_URL } from '@shared/data-access/api-url.token';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [{ provide: API_URL, useValue: 'https://api.betaseries.com' }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
